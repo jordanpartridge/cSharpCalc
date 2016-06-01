fd180539799e728157e526bb2b437815867735db
jordanpartridge <jordan.l.partridge@gmail.com>
2016-06-01 08:12:48 -0700

    initial commit

---
+++ WpfApplication2/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+
+{
+    class Expression
+    {
+        public Expression(double Val1, String Operation){
+            val1 = Val1;
+            operation = Operation;
+        }
+        private double val1;
+
+        public double Val1
+        {
+            get { return val1; }
+            set { val1 = value; }
+        }
+        private double val2;
+
+        public double Val2
+        {
+            get { return val2; }
+            set { val2 = value; }
+        }
+        private string operation;
+
+        public string Operation
+        {
+            get { return operation; }
+            set { operation = value; }
+        }
+        public double calc()
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "add":
+                    result =  val1 + val2;
+                    break;
+                case "subtract":
+                    result = val1 - val2;
+                    break;
+                case "divide":
+                    result = val1 / val2;
+                    break;
+                case "multiply":
+                    result = val1 * val2;
+                    break;
+               
+            }
+            return result;
+        }
+
+    }
+
+    
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Calculations;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        private Calculations.Expression myExpression;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void num_click(object sender, RoutedEventArgs e)
+        {
+            Button btnClicked = (Button)sender;
+            if (Output.Content.ToString() == "0")
+            {
+                Output.Content = btnClicked.Content;
+            }
+            else
+            {
+                Output.Content += btnClicked.Content.ToString();
+            }
+
+        }
+
+        private void dec_click(object sender, RoutedEventArgs e)
+        {
+            if (Output.Content.ToString().Contains("."))
+            {
+                //do nothing
+            } else{
+                Output.Content += ".";
+            }
+        }
+
+        private void Del_click(object sender, RoutedEventArgs e)
+        {
+            if(Output.Content.ToString().Length > 1)
+            {
+
+            Output.Content = Output.Content.ToString().Remove(Output.Content.ToString().Length - 1);
+            }
+            else
+            {
+                Output.Content = "0";
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Opperator_click(object sender, RoutedEventArgs e)
+        {
+            Button btnClicked = (Button)sender;
+            switch (btnClicked.Content.ToString())
+            {
+                case "+":
+                    myExpression = new Calculations.Expression(Double.Parse(Output.Content.ToString()), "add");
+                    Output.Content = "0";
+                    break;
+                case "-":
+                    myExpression = new Calculations.Expression(Double.Parse(Output.Content.ToString()), "subtract");
+                    Output.Content = "0";
+                    break;
+                case "/":
+                    myExpression = new Calculations.Expression(Double.Parse(Output.Content.ToString()), "divide");
+                    Output.Content = "0";
+                    break;
+                case "*":
+                    myExpression = new Calculations.Expression(Double.Parse(Output.Content.ToString()), "multiply");
+                    Output.Content = "0";
+                    break;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+   
+
+        private void equal_click(object sender, RoutedEventArgs e)
+        {
+            myExpression.Val2 = double.Parse(Output.Content.ToString());
+            Output.Content = myExpression.calc();
+        }
+
+      
+
+     
+       
+
+        
+
+
+
+
+
+
+    }
+}
